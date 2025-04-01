aeae762bcc68cd773ae4eed8bd30133d3e6b0cda
ziggiepez <jarrod.gibiser@yahoo.com>
2025-03-31 20:43:26 -0400

    initial comit
    added html for index and sub pages.
    added style.css
    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("moble-nav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("moble-nav").style.width = "0%";
+}
